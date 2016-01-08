9c54378a093cd6118bc3d889928a8bff8bf4e8dd
Max Holder <mxhold@gmail.com>
2016-01-08 15:26:27 -0800

    Add 404 for non-matching routes

--- lib/pastebin_ex.ex
+++ lib/pastebin_ex.ex
@@ -10,5 +10,9 @@ defmodule PastebinEx do
       page_contents = EEx.eval_file("lib/views/usage.html.eex", [base_url: base_url])
       send_resp(conn, 200, page_contents)
     end
+
+    match _ do
+      send_resp(conn, 404, "404")
+    end
   end
 end
